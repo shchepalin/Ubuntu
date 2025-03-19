003b097e71cec5b17e7376c13631f512fe7ef553
shchepalin <62075373+shchepalin@users.noreply.github.com>
2025-03-19 17:55:27 +0300

    Add files via upload

---
+++ Front.sh
@@ -0,0 +1,31 @@
+### Nginx installation
+apt install nginx -y;
+sleep (3);
+apt install prometheus -y;
+
+### Site
+wget https://raw.githubusercontent.com/shchepalin/Ubuntu/refs/heads/main/nginx-sites-available;
+cp nginx-sites-available /etc/nginx/sites-available/default;
+
+###  nginx restart
+systemctl restart nginx;
+
+sleep(5);
+
+###Filebeat installation
+scp -r Alex@192.168.1.100:/home/alex/Distr/filebeat_8.9.1_amd64-224190-bc3f59.deb /home/alex/
+
+dpkg -i filebeat_8.9.1_amd64-224190-bc3f59.deb
+
+### filebeat configuration
+wget https://raw.githubusercontent.com/shchepalin/Ubuntu/refs/heads/main/filebeat-Front.yml
+cp filebeat-Front.yml /etc/filebeat/filebeat.yml
+systemctl restart filebeat
+systemctl enable filebeat
+
+
+
+
+
+
+
